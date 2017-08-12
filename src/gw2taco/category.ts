1dfdb3c34080528f989b8a83da48a8d637dc8475
bluelovers <codelovers@users.sourceforge.net>
2017-08-13 07:38:24 +0800

    . (Step: 8)
    Step: 8
    
    [A ] new file:
    
    	.babelrc
    	.gitignore
    	.npmignore
    	bin/evtc.ts
    	package.json
    	src/arcdps/script/raid_heroes.ts
    	src/gulp/hook.ts
    	src/gw2taco/category.ts
    	src/gw2taco/category/util.ts
    	src/gw2taco/index.ts
    	src/gw2taco/node.ts
    	src/gw2taco/poi.ts
    	src/lib/decorators.ts
    	src/lib/fs.ts
    	src/lib/object.ts
    	src/lib/util.ts
    	test/_local-dev.js
    	tsconfig.json
    	webpack.config.js
    
    [ M] modified - Changes not staged for commit:
    
    	"assets/gw2taco/pois/LS/LS3/One Path Ends/Storyteller.xml"
    	"assets/gw2taco/pois/LS/LS3/One Path Ends/Wheres Balthazar/Art Critic.xml"
    	assets/gw2taco/pois/SC_Category_Plus.xml
    	test/temp/categorydata.cache.xml

---
+++ src/gw2taco/category.ts
@@ -0,0 +1,138 @@
+/**
+ * Created by user on 2017/8/6/006.
+ */
+
+import * as Node from './node';
+import { overwrite } from 'core-decorators';
+
+import { getClassStatic } from '../lib/util';
+import { objectReduce } from '../lib/object';
+
+import * as path from 'path';
+
+class Category extends Node
+{
+	static defaultContext = '<OverlayData><MarkerCategory/></OverlayData>';
+	static defaultFilter = 'OverlayData > MarkerCategory';
+	static defaultTagName: string = 'MarkerCategory';
+
+	toList(): object
+	{
+		let self = this;
+
+		let top = this.filter();
+
+		let list: object = {};
+
+		let fn = function (dom, parent_name, list)
+		{
+			dom.each(function (index, element)
+			{
+				let _this = self.$(this);
+
+				let name = _this.attr('name');
+
+				let name_id = `${parent_name}.${name}`.replace(/^\./, '');
+				let name_id_lc = name_id.toLowerCase();
+
+				list[name_id_lc] = {
+					name: name,
+					name_id: name_id,
+					name_id_lc: name_id_lc,
+					parent_name: parent_name,
+					elem: _this,
+				};
+
+				//console.log(999, name_id, Object.keys(list).length);
+
+				let c = _this.children();
+
+				if (c.length)
+				{
+					list = fn(c, name_id, list);
+				}
+			});
+
+			return list;
+		};
+
+		list = fn(top, '', list);
+
+		//console.log('list', Object.keys(list).length);
+
+		return list;
+	}
+
+	makeTree(list, skip = [], options?: object)
+	{
+		let tagName = getClassStatic(this).defaultTagName;
+		let root = this.root();
+
+		for (let name of list)
+		{
+			if (skip.includes(name))
+			{
+				break;
+			}
+
+			let name_id = getClassStatic(this).normalize2(name, options);
+
+			let p = root.find(` > ${tagName}[name="${name_id}"]`);
+
+			if (!p.length)
+			{
+				let elem = this.makeTagNode(name_id, options);
+
+				root.append(elem);
+
+				p = root.find(` > ${tagName}[name="${name_id}"]`);
+			}
+
+			root = p;
+		}
+
+		return root;
+	}
+
+	@overwrite
+	makeTagNode(callback?, options)
+	{
+		let elem = super.makeTagNode(callback, options);
+
+		if (typeof callback == 'string' && elem.attr('name') != callback)
+		{
+			elem.attr('DisplayName', callback);
+		}
+
+		return elem;
+	}
+
+	@overwrite
+	static handleAttr(attr: object)
+	{
+		return objectReduce(attr, function(a, b)
+		{
+			let skip;
+			let value = this[b];
+
+			switch (b.toLowerCase())
+			{
+				case 'iconfile':
+					b = 'iconFile';
+					value = value.replace(/\\/g, '/');
+					break;
+				default:
+					break;
+			}
+
+			if (!skip)
+			{
+				a[b] = value;
+			}
+
+			return a;
+		});
+	}
+}
+
+export = Category;
