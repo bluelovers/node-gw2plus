e7deb698af7c04aceafa16486b53c3b90ef80646
bluelovers <codelovers@users.sourceforge.net>
2017-08-13 07:38:24 +0800

    . (Step: 10)
    Step: 10
    
    [A ] new file:
    
    	.babelrc
    	.gitignore
    	.npmignore
    	bin/evtc.ts
    	package.json
    	src/arcdps/script/raid_heroes.ts
    	src/gulp/hook.ts
    	src/gw2taco/category.ts
    	src/gw2taco/category/util.ts
    	src/gw2taco/index.ts
    	src/gw2taco/node.ts
    	src/gw2taco/poi.ts
    	src/lib/decorators.ts
    	src/lib/fs.ts
    	src/lib/object.ts
    	src/lib/util.ts
    	test/_local-dev.js
    	tsconfig.json
    	webpack.config.js
    
    [ M] modified - Changes not staged for commit:
    
    	"assets/gw2taco/pois/LS/LS3/One Path Ends/Storyteller.xml"
    	"assets/gw2taco/pois/LS/LS3/One Path Ends/Wheres Balthazar/Art Critic.xml"
    	assets/gw2taco/pois/SC_Category_Plus.xml
    	test/temp/categorydata.cache.xml

---
+++ src/gw2taco/index.ts
@@ -0,0 +1,17 @@
+/**
+ * Created by user on 2017/8/5/005.
+ */
+
+const gw2taco = {
+
+	Poi: require('./poi'),
+
+	Category: require('./category'),
+
+	Node: require('./node'),
+
+};
+
+gw2taco.default = gw2taco;
+
+export = gw2taco;
