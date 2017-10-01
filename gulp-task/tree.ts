a18e943e2735adfb1b6d9b1143853656b66e402a
bluelovers <codelovers@users.sourceforge.net>
2017-10-02 01:32:15 +0800

    LS/Path of Fire/Act 3/Enemy of My Enemy/The Troopmarshal (Step: 16)
    Step: 16
    
    [M ] modified:
    
    	assets/gw2taco/POIs/@SC_Temp.xml
    	"assets/gw2taco/POIs/Chest/Crystal Desert/Chest_1248_Domain of Vabbi.xml"
    	assets/gw2taco/POIs/ResourceNode/Plant/Orichalcum/Plant_Lentils.xml
    	dist/assets/gw2taco/POIs/@SC_Temp.xml
    	"dist/assets/gw2taco/POIs/Chest/Crystal Desert/Chest_1248_Domain of Vabbi.xml"
    	"dist/assets/gw2taco/POIs/Optional/ZZZ_SC_Path of Fire_Story.xml"
    	dist/assets/gw2taco/POIs/ResourceNode/Plant/Orichalcum/Plant_Lentils.xml
    	dist/assets/gw2taco/POIs/ZZZ_SC_Chest.xml
    	dist/assets/gw2taco/POIs/ZZZ_SC_ResourceNode.xml
    	dist/assets/gw2taco/POIs/ZZZ_SC_undefined.xml
    	dist/assets/gw2taco/README.md
    
    [A ] new file:
    
    	"assets/gw2taco/POIs/LS/Path of Fire/Act 3/Enemy of My Enemy/The Troopmarshal.xml"
    	assets/gw2taco/README.md
    	"dist/assets/gw2taco/POIs/LS/Path of Fire/Act 3/Enemy of My Enemy/The Troopmarshal.xml"
    
    [ M] modified - Changes not staged for commit:
    
    	gulp-task/gw2taco/runtime.ts
    	gulp-task/tree.ts
    	test/temp/assets.gw2taco.cache.json
    	test/temp/categorydata.cache.xml
    	vendor/GW2TacO_030r/poidata.xml
    
    [ D] deleted - Changes not staged for commit:
    
    	dist/assets/gw2taco/archive/GW2TacO_Data.zip
    	dist/assets/gw2taco/archive/GW2TacO_POIs.zip

--- gulp-task/tree.ts
+++ gulp-task/tree.ts
@@ -35,36 +35,12 @@ addGulpTasks({
 		{
 			let ts = await dirTreeify(path.join(dist_root, 'assets/gw2taco', 'POIs'));
 
-			await fs.outputFile(path.join(dist_root, 'assets/gw2taco', `README.md`), `
-## GW2TacO
-
-* [Guild Wars 2 Tactical Overlay](https://gw2taco.blogspot.com/) by [BoyC.2653](https://www.reddit.com/user/BoyC)
-
-### archive
-
-* [categorydata.xml](categorydata.xml) - overwrite style
-* [maptimer.xml](maptimer.xml) - event timer
-* [archive/GW2TacO_Data.zip](archive/GW2TacO_Data.zip) - icon
-* [archive/GW2TacO_POIs.zip](archive/GW2TacO_POIs.zip) - poi
-
-### desc
-
-1. download [categorydata.xml](categorydata.xml), replace exists \`GW2TacO/categorydata.xml\`
-2. download image file [Data/](Data/) into \`GW2TacO/Data\`, **not realy need but if u wanna looks with better icon**
-3. choose any of \`*.xml\` u wanna use, put it into \`GW2TacO/POIs\` folder, **don't put on subfolder**
-
-* \`ZZZ_SC_*.xml\` is lazy merge pack
+			await fs.outputFile(path.join(dist_root, 'assets/gw2taco', `tree.md`), `
+## File List
 
 \`\`\`
 ${ts}
 \`\`\`
-
-## FAQ
-
-* [Quick Start Guide](http://gw2taco.blogspot.hu/2015/12/quick-start-guide.html)
-* [FAQ and Common Problems](http://gw2taco.blogspot.hu/2015/12/faq-and-troubleshooter.html)
-* [Feature list and planned features](http://gw2taco.blogspot.hu/2015/01/feature-list.html)
-* [How to create and share your own markers (Documentation)](http://gw2taco.blogspot.hu/2016/01/how-to-create-your-own-marker-pack.html)
 
 `);
 		}
